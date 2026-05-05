ffe477c693800ad18d70c35ca5985a7c44e9dab7
PurpleAnubis <kres.edo@proton.me>
2026-05-05 21:36:55 +0200

    Removing comments.

--- asteroid.py
+++ asteroid.py
@@ -22,20 +22,15 @@ class Asteroid(CircleShape):
 
         log_event("asteroid_split")
 
-        # Generate random angle between 20 and 50 degrees
         random_angle = random.uniform(20, 50)
 
-        # Create new velocity vectors by rotating current velocity
         velocity1 = self.velocity.rotate(random_angle)
         velocity2 = self.velocity.rotate(-random_angle)
 
-        # Calculate new radius for smaller asteroids
         new_radius = self.radius - ASTEROID_MIN_RADIUS
 
-        # Create two new asteroids at current position
         asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
         asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
 
-        # Set velocities, scaled up by 1.2 for faster movement
         asteroid1.velocity = velocity1 * 1.2
         asteroid2.velocity = velocity2 * 1.2
